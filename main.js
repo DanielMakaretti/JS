b46980a20028268102005ce3c08af27088bb223b
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-08-26 17:18:28 +0300

    beginner

---
+++ main.js
@@ -0,0 +1,22 @@
+let userName = 'Daniel';
+let userAge = '25';
+let userHeight;
+console.log(userName + ' is ' + userAge + ' years old');
+userHeight = 170;
+console.log(userHeight);
+console.log (`2 + 2 = ${2+2}`);
+let nameFiledChecked = true;
+let ageFiledChecked = false;
+let a = 1;
+let c = 2;
+console.log (c);
+let f = 1+2;
+let d = 1+3;
+console.log (d);
+const myAge = 25;
+console.log (myAge);
+const REDCOLOR = '#F00';
+const ORANGECOLOR = '#FR20';
+const BLUECOLOR = '#RGB5'
+let color = REDCOLOR;
+console.log (color);
