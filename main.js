78b222d041d960dd6b451c95388b96f77b45cbef
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-08-28 18:34:44 +0300

    study

--- main.js
+++ main.js
@@ -38,5 +38,47 @@ showMessage();
 showMessage();
 showMessage();
 
+let s = 5;
+let x = 6;
 
+console.log ('Сложение: ' + (x % s));
   
+let num_3 = Number('5');
+let num_4 = Number('3');
+console.log (num_3 + num_4);
+
+console.log ('Math: ' + Math.PI);
+console.log ('Math: ' + Math.E);
+console.log ('Math: ' + Math.sin(num_3));
+console.log ('Math: ' + Math.min(3, 4, 5, 7, 1));
+
+let number1 = 15;
+let number2 = true;
+if (number1 == 15 && number2 == true) {
+    console.log ('No, bitch');
+}
+else if (number1 >= 16) {
+    console.log ('No, gay');
+}
+else if (number1 < 10) {
+    console.log ('SEX.gay')
+}
+else {
+    console.log ('XAXAXA,lox')
+}
+
+let lesson = 2 + 2;
+
+switch (lesson) {
+    case 3:
+        console.log('Minor');
+        break;
+        case 4:
+            console.log('Oh, yes, bro! You are right!');
+            break;
+            case 10:
+                console.log('Minor, meen');
+                break;
+                default:
+                    console.log('None meaning');
+}
