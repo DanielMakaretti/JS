9f4cd8c4d8d14ceee075f7f4914785989a8c0735
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-09-01 16:32:48 +0300

    Update 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let userName = 'Daniel';
+/* let userName = 'Daniel';
 let userAge = '25';
 let userHeight;
 console.log(userName + ' is ' + userAge + ' years old');
@@ -19,7 +19,7 @@ const REDCOLOR = '#F00';
 const ORANGECOLOR = '#FR20';
 const BLUECOLOR = '#RGB5'
 let color = REDCOLOR;
-console.log (color);
+console.log (color); */
 /* alert(a>c);
 let result = 5 > 4;
 alert( result ); */
@@ -30,7 +30,7 @@ alert( result ); */
   (login == 'Директор') ? 'Здравствуйте':
   (login == '') ? 'Нет логина':
   ''; */
- function showMessage() {
+ /* function showMessage() {
 console.log ('Сообщение!');
  }
 showMessage();
@@ -81,7 +81,7 @@ switch (lesson) {
                 break;
                 default:
                     console.log('None meaning');
-}
+} */
 
 /* if (browser == 'Edge') {
     alert( "You've got the Edge!" );
@@ -93,7 +93,7 @@ else {
     alert( 'We hope that this page looks ok!' );
 } */
 
-const number = +prompt('Введите число между 0 и 3', '');
+/* const number = +prompt('Введите число между 0 и 3', '');
 
 switch (number) {
     case 0:
@@ -108,4 +108,69 @@ switch (number) {
                 break;
                 default:
                     alert('Buy');
+} */
+/* 
+let array = [5, true, 'stroka', 5.7, 0, -100];
+console.log(array[0]);
+if (array[0] > array[3]) {
+    console.log ('Hi,mommy!')
 }
+else {
+    console.log ('NO!');
+}
+
+let matrix = [
+    [5, true], ['stroka', 5.7], [0, -100]
+];
+console.log(matrix[0][1]); */
+
+/* for (var i = 10; i < 50; i *= 2) {
+console.log (i);
+}
+
+var j = 1000;
+while (j > 100) {
+    console.log (j);
+    j -= 100;
+}
+*/
+/* 
+for (var i = 10; i < 20; i += 2) {
+    if (i > 15)
+    break;
+    console.log (i);
+}
+
+var arr = [5, true, 'stroka', 5.7, 0, -100];
+
+for (var i = 0; i < arr.length; i++) {
+    arr[i] *= -2;
+    console.log('Элемент ' + (i + 1) + ': ' + arr[i]);
+} */
+
+function info(word) {
+   
+    console.log(word + 'Hi!');
+    console.log('Hi!!!!!');
+}
+function summa(a,b) {
+    var res = a + b;
+    info(res);
+
+
+}
+summa(5, 7);
+
+function summa(arr) {
+    var sum = 0;
+    for(var i=0; i < arr.length; i++) 
+sum += arr[i];
+    console.log(sum);
+}
+var array = [6, 8, 1];
+var array_2 = [6, 8, 1, 7];
+var array_3 = [6, 8, 1, 90];
+
+summa(array);
+summa(array_2);
+summa(array_3);
