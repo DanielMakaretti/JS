c7da9c7e646694bfc48b87013225d27f2cb54c66
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-09-16 10:29:27 +0300

    Calculator by using switch method

--- main.js
+++ main.js
@@ -1,51 +1,18 @@
 
-  const allist = {
-    list: {
-    'create a new practicle task': 'In Progress',
-    'make a bed': 'Done',
-    'write a post': 'To Do',
-    },
-  };
-/* function changeStatus(task, status) {
-    If (task in allist.list) {
-        allist.list[task] = status;
-        } else {
-        console.log("нет такой задачи");
-        }
-    }
-  changeStatus(); */
-  function addTask(task, status) {
-    allist.list[task] = status;
-    console.log( allist.list);
-  }
-  addTask();
-
-  function deleteTask(task) {
-    delete allist.list[task];
-  }
-  deleteTask();
-
-
-/* function showVerticalMessage() {
-    let strada = `
-    s
-    t
-    r
-    a
-    d
-    a`
-    console.log(strada);
-}
-showVerticalMessage(strada); */
-
-
 /*=================================Calculator==================================================*/ 
 
 function calc(operation, a, b) {
- if (operation == 'add') {return (a + b)}
- else if (operation == 'multi') {return (a * b)}
- else if (operation == 'subtract') {return (a - b)}
-else { return confirm('It is wrong!');} 
+  switch(operation) {
+    case 'add':
+      return (a + b)
+  
+  case 'multi':
+    return (a - b);
+    case 'subtract':
+      return (a * b);
+      default:
+        return alert('It is wrong!');
+  }
 }
 console.log(calc('subtract', 1, 2));
 
