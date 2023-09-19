4d0287401e53a5e1ae7a6aa33d99b8f586c43a27
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-09-19 21:00:15 +0300

    To Do list

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
+const allist = {
+  list: {
+    "create a new practice task": "In Progress", 
+    "make a bed": "Done", 
+    "write a post": "To Do",
+  },
+};
+function changeStatus(key, status) {
+  if (key in allist.list) {
+    allist.list[key] = status;
+    console.log('Статус задачи: ' + key + ' изменен на - ' + status );
+}
+else {
+  console.log('задачи - ' + key + ', со статусом - ' + status + ' нет в списке');
+}
+}
 
-/*=================================Calculator==================================================*/ 
+function addTask(key, status) {
+  allist.list[key] = status;
+  console.log('Задача ' + key + ', со статусом - ' + status + ', добавлена');
+}
 
-function calc(operation, a, b) {
-  switch(operation) {
-    case 'add':
-      return Number(a) + Number(b);
-  case 'multi':
-    return Number(a) - Number(b);
-    case 'subtract':
-      return Number(a) * Number(b);
-      default:
-        return alert('It is wrong!');
+function deleteTask(key, status) {
+  if (key in allist.list) {
+    allist.list[key] = status;
+    delete status;
+console.log('Задача ' + key + ', со статусом - ' + status + ', удалена');
+  }
+  else {
+    console.log('задачи - ' + key + ', со статусом - ' + status + ' нет в списке');
   }
 }
-console.log(calc('subtract', 2, 2)); //4
-console.log(calc('multi', 1, 2)); //-1
-console.log(calc('add', 1, 2)); //3
-
-/*==============calc №2==================================================*/
-function calc2(operation, a, b) {
-  let result;
-  switch(operation) {
-    case 'add':
-    result = Number(a) + Number(b);
-    break;
-    case 'multi':
-      result = Number(a) - Number(b);
-    break;
-    case 'subtract':
-      result = Number(a) * Number(b);
-    break;
-    default:
-      return console.log('It is wrong!');
-  }
-  console.log(result);
+function showList() {
+  
 }
-calc2('add', 1, 2); //3
-calc2('multi', 1, 2); //-1
-calc2('subtract', 1, 2); //2
+addTask('wash the floors', 'Done');
+changeStatus('create a new practice task', 'To Do');
+deleteTask('wash the floors', 'Done');
+
