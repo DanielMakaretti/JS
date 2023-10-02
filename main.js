8f88596d32fbec51356f45da1d7a8c2d63b68858
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-10-02 20:14:45 +0300

    To do list on massive end

--- main.js
+++ main.js
@@ -3,62 +3,37 @@ const list = [
   {name: 'test', status: 'Done', priority: 'high'} 
 ];
 
-  list.forEach((object, index) => {
-    if(object.name === 'create a post') {
-    let NewDone = list[index].status;
-    NewDone = 'Done';
-     let NewHigh = list[index].priority;
-     NewHigh = 'high';
-      
-    }
-    else if(object.name === 'test'){
-      NewProgress = list[index].status;
-      NewProgress = 'It Progress';
-    }
-  });
 
-    const NewArray2 = list.splice(2, 0, {
-      name:'sex',
-      status: 'Done',
-      priority: 'medium'
-    });
-    console.log(NewArray2);
-  /*   const NewArray = list.map(task => {
-      if(task === 'sex') {
-        return {
-          name: 'sex',
-          status: 'Done',
-          priority: 'medium'
-        }
-      }
-        else if(task === 'test') {
-          return {
-            name: 'test',
-            status: 'Done',
-            priority: 'high'
-          }
-        }
-        else {
-          return {
-            name: 'create a post',
-            status: 'In progress',
-            priority: 'low'
-          }
-        }
-      });
-      
-      console.log(NewArray); */
   
- 
-   
- 
-   /*function deleteTask(index, numeric) {
-    let result = addTask(2, 1, 'sex');
-      let deletetask = result.splice(index, numeric);
-      if(deleteTask = result.splice(index, numeric)){
-      console.log(deletetask);
+let NewArray = {name: 'sex', status: 'Done', priority: 'medium'};
+let NewArray2 = {name: 'horror', status: 'Done', priority: 'high'};
+list.push(NewArray, NewArray2);
+     
+    
+    const changeStatus =  list.map(object => {
+      if (object.name === 'create a post') {
+        return {
+          name: object.name,
+          status: 'Done',
+          priority: 'high'
+        };
+      }
+      else if(object.name === 'test') {
+        return {
+          name: object.name,
+          status: 'In Progress',
+          priority: 'medium'
+        };
       }
       else {
-        console.log('Таких объектов в массиве нет');
+        return object;
       }
-    };  */
+     })
+     
+
+  list.splice(3, 3);
+
+
+  for (let task of list) {
+    console.log(task);
+  }
