8bfbf21db7ddfba9219ce52cc91fd17a99021f5b
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-10-05 21:05:43 +0300

    Timeout

--- main.js
+++ main.js
@@ -1,55 +1,15 @@
-const list = [ 
-	{name: 'create a post', status: 'In progress', priority: 'low'}, 
-  {name: 'test', status: 'Done', priority: 'high'} 
-];
-
-
+function BackCountTimeout(second){
+   console.log(second);
   
-let NewArray = {name: 'sex', status: 'Done', priority: 'medium'};
-let NewArray2 = {name: 'horror', status: 'Done', priority: 'high'};
-list.push(NewArray, NewArray2);
-     
-    
-    const changeStatus = list.map(object => {
-      switch(object.name) {
-        case 'create a post':
-          return {
-            name: object.name,
-            status: 'Done',
-            priority: 'high'
-          };
-         
-          case 'test':
-            return {
-              name: object.name,
-              status: 'In Progress',
-              priority: 'medium'
-            };
-           
-            case 'sex':
-            return {
-              name: object.name,
-              status: 'In Progress',
-              priority: 'low'
-            };
-            
-            case 'horror':
-            return {
-              name: object.name,
-              status: 'In Progress',
-              priority: 'low'
-            };
-           
-            default:
-              console.log('Invalid task name');
-              return object;
-      }
-      
-    });
-    
-     
-  /*   changeStatus.shift()
-     changeStatus.pop(); */
-  for (let task of changeStatus) {
-    console.log(task);
-  }
+let counterId = setInterval(() => {
+   --second
+   if(second === 0){
+      clearInterval(counterId);
+   }
+   console.log(second);
+}, 500);
+};
+
+
+
+BackCountTimeout(10)
