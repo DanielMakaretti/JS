d7a4c0413b7db917e52c82bea4de481c192a8436
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-10-10 16:56:30 +0300

    calculator form

--- main.js
+++ main.js
@@ -1,36 +1,6 @@
-const button = document.getElementById('elem');
-const pauseButton = document.getElementById('elem2');
-const resumeButton = document.getElementById('elem3');
-let isPaused = false;
-let Stopwatch;
-let watch = 0;
-const buttonClickStopwatch = () => {
-    if(isPaused === true){
-        return;
-    };
-    Stopwatch = setInterval(() => {
-                watch++;
-        if(watch === 10){
-            clearInterval(Stopwatch);
-        };
-       
-    console.log(watch);
-}, 1000);
-};
-const togglePause = () => {
-    isPaused = !isPaused;
-    if(isPaused){
-        clearInterval(Stopwatch);
-} else{
-    buttonClickStopwatch();
-};
-};
+const Equally = document.getElementById('Equally');
 
-const toggleResume = () => {
-    isPaused = false;
-    buttonClickStopwatch();
+function buttonClickEqually(){
+    console.log(10);
 };
-button.addEventListener('click', () => buttonClickStopwatch());
-pauseButton.addEventListener('click', () => togglePause());
-resumeButton.addEventListener('click', () => toggleResume());
-
+Equally.addEventListener('click', buttonClickEqually);
