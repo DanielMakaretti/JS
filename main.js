c53f44cdb96d4bed39c4828035306fcfcc7a6c13
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-10-14 12:28:53 +0300

    calculator check

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let minus = document.getElementById('minus').value;
 let multi = document.getElementById('multi').value;
 let division = document.getElementById('division').value;
 const result = document.getElementById('result');
+elem = document.getElementById('form');
+const backgroundColor = elem.style.backgroundColor;
     if(select === plus){
         const plus_exp = calc_label + calc_label2;
         result.textContent = plus_exp;
@@ -38,8 +40,39 @@ resultat.addEventListener('click', getCalculator);
 
 
 });
+function validation(form){
+  function removeError(input){
+const parent = input.parentNode;
+if(parent.classList.contains('error')){
+  parent.querySelector('.error-label').remove()
+  parent.classList.remove('error')
+}
+  }
 
-document.getElementById('form').addEventListener('submit', function buttonClickEqually(event) {
+  function createError(input, text){
+    const parent = input.parentNode;
+    const errorLabel = document.createElement('label')
+    errorLabel.classList.add('error-label');
+    errorLabel.textContent = text;
+    parent.append(errorLabel);
+    parent.classList.add('error');
+    
+  }
+  let resultTwo = true;
+  for(const input of form.querySelectorAll('input')){
+    removeError(input)
+    if(input.value == ''){
+      console.log('ERROR');
+      createError(input, 'Поле не заполнено');
+      resultTwo = false;
+    }
+  }
+  return resultTwo;
+}
+
+document.getElementById('form').addEventListener('submit', function (event) {
  event.preventDefault()
-   validation(this);
+  if(validation(this) == true){
+    alert('Form successfully sent');
+  }
 });
