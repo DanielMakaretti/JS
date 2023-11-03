dcdb8c2e5cece7015ea48399857aa70cca493192
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-11-03 14:00:56 +0300

    # 47 add task in JSON
    When adding a task if were used for and array.map() methods

--- main.mjs
+++ main.mjs
@@ -1,33 +1,15 @@
-const data = {
-    "users": [
-      {
-        "firstName": "Dennis",
-        "lastName": "Ritchie",
-        "dateOfBirth": "September 9, 1941",
-        "knowsAs": "created the C programming language"
-      },
-      {
-        "firstName": "Steve",
-        "lastName": "Jobs",
-        "dateOfBirth": "February 24, 1955",
-        "knowsAs": "co-founder of Apple Inc, Next, Pixar"
-      },
-      {
-        "firstName": "Bill",
-        "lastName": "Gates",
-        "dateOfBirth": "February 24, 1955",
-        "knowsAs": "co-founder of Microsoft"
-      },
-      {
-        "firstName": "Alan",
-        "lastName": "Turing",
-        "dateOfBirth": "June 23, 1912",
-        "knowsAs": "developed the first modern computer"
-      }
-    ]
-  }
-  
+import data from './data.json' assert {type: "json"};
+const dataUsers = data.users;
+console.log(dataUsers)
+const totality = dataUsers.map(function(dataUsers){
+    return console.log(`${dataUsers.lastName}, born at ${dataUsers.dateOfBirth}, and ${dataUsers.knowsAs},`);
+}, 0)
+
+for (let key in dataUsers) {
+    console.log(`${dataUsers[key].lastName}, born at ${dataUsers[key].dateOfBirth}, and ${dataUsers[key].knowsAs},`);
+}
 
 
-const result = JSON.parse(data);
-console.log(result);
+for (let i = 0; i < dataUsers.length; i++){
+    console.log(`${dataUsers[i].lastName}, born at ${dataUsers[i].dateOfBirth}, and ${dataUsers[i].knowsAs},`)
+}
