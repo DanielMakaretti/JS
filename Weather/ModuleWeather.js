c0103307c4b06e9df70096144490e38fc69c4c09
Daniel Makaretti <139051146+DanielMakaretti@users.noreply.github.com>
2023-11-15 21:14:10 +0300

    Weather add icons

--- Weather/ModuleWeather.js
+++ Weather/ModuleWeather.js
@@ -1,65 +1,79 @@
 
+export { createAddElement, moduleThenData, deleteTextForm, deleteCityLocation };
+
 const createAddElement = () => {
-    const cityLocations = document.getElementById('city_locations-inner');
-    const locationText = document.getElementById('degree_location-text');
+  const cityLocations = document.getElementById('city_locations-inner');
+  const locationText = document.getElementById('degree_location-text');
   const saveResult = document.createElement('li');
-    const closeButton = document.createElement('button');
-    closeButton.classList = 'closeButton';
-    closeButton.id = 'closeButton';
-    closeButton.setAttribute('role', 'button');
-    cityLocations.appendChild(saveResult);
-    saveResult.appendChild(closeButton);
-    closeButton.addEventListener('click', deleteCityLocation);
-    // const naturalButton = document.createElement('button');
-    // naturalButton.id = 'naturalButton';
-    
+  const closeButton = document.createElement('button');
+  closeButton.classList = 'closeButton';
+  closeButton.id = 'closeButton';
+  closeButton.setAttribute('role', 'button');
+  cityLocations.appendChild(saveResult);
+  saveResult.appendChild(closeButton);
+  closeButton.addEventListener('click', deleteCityLocation); //click delete button
   saveResult.id = 'saveResult';
   saveResult.classList = 'saveResult';
-  if(locationText.innerText !== '') {
+  if (locationText.innerText !== '') {
     const innerTextLocation = document.createTextNode(locationText.innerText);
     saveResult.appendChild(innerTextLocation);
     cityLocations.appendChild(saveResult);
   };
 };
+
 const deleteCityLocation = () => {
   const deleteParent = document.getElementById('city_locations-inner');
   const deleteChild = document.querySelector('li');
   if (deleteChild) {
-    
+
     deleteParent.removeChild(deleteChild);
   };
 };
 const moduleThenData = (data) => {
-    console.log(data)
-    const degree = document.getElementById('degree_location-text');
-    const temperature = document.getElementById('temperature');
-    const formulaDegree = (data.main.temp) - 273.15; //convert kelfins in degrees Celsius by formula C = x - K
-    const resultFormula = (Math.trunc(formulaDegree) + '°');//reset to the lowest value
-    const textTemperature = document.createElement('p');//create new element - paragraph
-    textTemperature.id = 'textTemperature';//set the id
-    textTemperature.classList = 'textTemperature';
-    textTemperature.innerHTML = resultFormula;//enter the temperatura data
-    const textCity = document.createTextNode(data.name);
-    textCity.id = 'textCity';
-    degree.appendChild(textCity);
-    temperature.appendChild(textTemperature);//add result temperature in block
-  };
-
-  const deleteTextForm = () => {
-    const input = document.getElementById('input');
-    input.addEventListener('input', function () {
-        const textTemperature = document.getElementById('temperature');
-        const textDegree = document.getElementById('degree_location-text');
-        const cityName = document.querySelector('.weather_input').value;
-        if (cityName === '') {
-            textTemperature.textContent = '';
-            textDegree.textContent = '';
-        };
-        });
+  console.log(data)
+  const degree = document.getElementById('degree_location-text');
+  const temperature = document.getElementById('temperature');
+  const formulaDegree = (data.main.temp) - 273.15; //convert kelfins in degrees Celsius by formula C = x - K
+  const resultFormula = (Math.trunc(formulaDegree) + '°');//reset to the lowest value
+  const textTemperature = document.createElement('p');//create new element - paragraph
+  textTemperature.id = 'textTemperature';//set the id
+  textTemperature.classList = 'textTemperature';
+  textTemperature.innerHTML = resultFormula;//enter the temperatura data
+  const textCity = document.createTextNode(data.name);
+  textCity.id = 'textCity';
+  degree.appendChild(textCity);
+  temperature.appendChild(textTemperature);//add result temperature in block
 };
 
-  
- 
+const deleteTextForm = () => {
+  const input = document.getElementById('input');
+  input.addEventListener('input', function () {
+    const textTemperature = document.getElementById('temperature');
+    const textDegree = document.getElementById('degree_location-text');
+    const imgWeather = document.getElementById('newImgSnow');
+    const newTextCloudy = document.getElementById('newTextSnow');
+    const imgClouds = document.getElementById('newImgClouds');
+    const textClouds = document.getElementById('newTextClouds');
+    const newImgRain = document.getElementById('newImgRain');
+    const newTextRain = document.getElementById('newTextRain');
+    const newImgClear = document.getElementById('newImgClear');
+    const newTextClear = document.getElementById('newTextClear');
+    const cityName = document.querySelector('.weather_input').value;
+    if (cityName === '') {
+      textTemperature.textContent = '';
+      textDegree.textContent = '';
+      newImgClear.remove();
+      newTextClear.remove();
+      newImgRain.remove();
+      newTextRain.remove();
+      imgClouds.remove();
+      textClouds.remove();
+      newTextCloudy.remove();
+      imgWeather.remove();
+    };
+  });
+};
 
 
-  export {createAddElement, moduleThenData, deleteTextForm, deleteCityLocation};
+
+
--- Weather/weather.js
+++ Weather/weather.js
@@ -1,47 +1,102 @@
-import {createAddElement, moduleThenData, deleteTextForm, deleteCityLocation} from './ModuleWeather.js';
+import { createAddElement, moduleThenData, deleteTextForm, deleteCityLocation } from './ModuleWeather.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const deleteCityLocation = () => {
-        const deleteParent = document.getElementById('city_locations-inner');
-        const deleteChild = document.querySelector('li');
-        if (deleteChild) {
-          console.log(deleteParent);
-          deleteParent.removeChild(deleteChild);
-        }
-      }
-const closeButton = document.getElementById('button_test');//delete CityLocation
-closeButton.addEventListener('click', deleteCityLocation);
-})
+    const closeButton = document.getElementById('button_test');//delete CityLocation
+    closeButton.addEventListener('click', deleteCityLocation);
+});
 const imgButton = document.getElementById('degree_location-inner');
 if (imgButton) {
-imgButton.addEventListener('click', createAddElement);
+    imgButton.addEventListener('click', createAddElement);
 };
-deleteTextForm(); 
+deleteTextForm();
 const findCityWeather = (event) => {
     event.preventDefault()
-const cityName = document.querySelector('.weather_input').value;
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const apiKey = '6bc43889ab3c419f7b6e87dc64a454c8';
-const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
-fetch(url)
-.then(response => {
-    if (!response.ok || response.status === 404) {
-        throw new Error('Request failed');
-    }
-    return response.json()
-   })
-.then(data => {
-   moduleThenData(data)
-})
-.catch(error => {
-   console.error('Ошибка, ебланка:', error)
-})
+    const cityName = document.querySelector('.weather_input').value;
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const apiKey = '6bc43889ab3c419f7b6e87dc64a454c8';
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+    fetch(url)
+        .then(response => {
+            if (!response.ok || response.status === 404) {
+                throw new Error('Request failed');
+            }
+            return response.json()
+        })
+        .then(data => {
+            moduleThenData(data)
+            changeWeatherSnow(data)
+            changeWeatherСloudy(data)
+            changeWeatherRain(data)
+            changeWeatherClear(data)
+        })
+        .catch(error => {
+            console.error('Ошибка, ебланка:', error)
+        })
 }
 const weatherForm = document.getElementById('weather_form');
 weatherForm.addEventListener('submit', findCityWeather);
 
+const changeWeatherSnow = (data) => {
+    if (data.snow) {
+        const rainless = document.getElementById('img_cloud');
+        const newElementRain = document.createElement('img');
+        const newTextElement = document.createElement('p');
+        newTextElement.classList = 'newImgClear';
+        newTextElement.textContent = 'Snow';
+        newElementRain.classList = 'newImgSnow';
+        newTextElement.id = 'newTextSnow';
+        newElementRain.id = 'newImgSnow';
+        newElementRain.src = '/Weather/weather_snow2.png';
+        rainless.appendChild(newElementRain);
+        rainless.appendChild(newTextElement);
+    };
+    };
+   
+const changeWeatherСloudy = (data) => {
+    if (data.weather[0].main == 'Clouds') {
+        const rainless = document.getElementById('img_cloud');
+        const newElementRain = document.createElement('img');
+        const newTextElement = document.createElement('p');
+        newTextElement.classList = 'newImgClear';
+        newTextElement.textContent = 'Clouds';
+        newElementRain.classList = 'newImgCloudy';
+        newTextElement.id = 'newTextClouds';
+        newElementRain.id = 'newImgClouds';
+        newElementRain.src = '/Weather/cloud.svg';
+        rainless.appendChild(newElementRain);
+        rainless.appendChild(newTextElement);
+    };
+};
 
+const changeWeatherRain = (data) => {
+    if (data.weather[0].main == 'Rain') {
+        const rainless = document.getElementById('img_cloud');
+        const newElementRain = document.createElement('img');
+        const newTextElement = document.createElement('p');
+        newTextElement.classList = 'newImgClear';
+        newTextElement.textContent = 'Rain';
+        newElementRain.classList = 'newImgCloudy';
+        newElementRain.id = 'newImgRain';
+        newTextElement.id = 'newTextRain';
+        newElementRain.src = '/Weather/weather_light_rain.png';
+        rainless.appendChild(newElementRain);
+        rainless.appendChild(newTextElement);
+    };
+};
 
-    
+const changeWeatherClear = (data) => {
+    if (data.weather[0].main == 'Clear') {
+        const rainless = document.getElementById('img_cloud');
+        const newElementRain = document.createElement('img');
+        const newTextElement = document.createElement('p');
+        newTextElement.classList = 'newImgClear';
+        newTextElement.textContent = 'Clear';
+        newElementRain.classList = 'newImgCloudyClear';
+        newTextElement.id = 'newTextClear';
+        newElementRain.id = 'newImgClear';
+        newElementRain.src = '/Weather/weather_clear.png';
+        rainless.appendChild(newTextElement);
+        rainless.appendChild(newElementRain);
+    };
+}
 
-    
- 
